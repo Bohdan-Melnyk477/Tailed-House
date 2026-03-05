35ab66cdc5d6592ad526c008d77daf50712a3431
Tymofii Danilov <timadanilov2002@gmail.com>
2026-03-05 16:51:42 +0100

    add stars visualisation, add nav buttons, add styles, fix positioning

--- src/js/success-stories-section.js
+++ src/js/success-stories-section.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import Raty from 'raty-js';
+import 'raty-js/src/raty.css';
 
 const storiesList = document.querySelector('#stories-list');
 
@@ -22,25 +25,47 @@ function createMarkup(feedbacks) {
     .map(
       item => `<div class="swiper-slide">
     <div class="feedback-card">
-      <div class="stars">${item.rate}</div>
+      <div class="stars js-raty" data-score="${item.rate}"></div>
       <p class="card-description">${item.description}</p>
       <h3 class="card-autor">${item.author}</h3>
       </div>
     </div>`
-    )
-    .join('');
+    ).join('');
   storiesList.insertAdjacentHTML('beforeend', markup);
   initSwiper();
+
+  const stars = document.querySelectorAll(".js-raty");
+  stars.forEach(el => {
+    try {
+      const score = el.dataset.score;
+      const raty = new Raty(el, {
+      readOnly: true,
+      score: score,
+      halfShow: true,
+      starOff: '../svg/star-outline.svg',
+      starOn: '../svg/star-filled.svg',
+      starHalf: '../svg/star-half.svg',
+    });
+    raty.init();
+    } catch (error) {
+      console.error("Raty error:", err);
+    }
+  });
 }
 
 function initSwiper() {
   const swiper = new Swiper('.feedbacks-slider', {
-    modules: [Pagination],
+    modules: [Pagination, Navigation],
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pag',
       clickable: true,
       dynamicBullets: true,
       dynamicMainBullets: 3,
+    },
+    navigation: {
+        nextEl: ".swiper-next-button",
+        prevEl: ".swiper-prev-button",
+        disabledClass: "slider-button-disabled",
     },
     breakpoints: {
       320: { slidesPerView: 1 },
